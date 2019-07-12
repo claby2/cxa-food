86c69657980f846936979248e8b10ff546116bd7
arrrc <40910186+arrrc@users.noreply.github.com>
2019-07-12 16:11:00 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -14,4 +14,17 @@ user.on("value", ()=>{
     console.log("user added")
 })
 
-// user.push({'Craig':'123456'})
+// user.push({'Craig':'123456
+
+//Storage
+let file = "https://anacortesoilandvinegarbar.com/wp-content/uploads/2015/11/apple.jpg"
+let storage = firebase.storage()
+let storageRef = storage.ref('food.jpeg')
+
+let imageBlob = new Blob(['./food.jpeg'],{type:'image/jpeg' })
+
+storageRef.put(imageBlob).then(function (snapshot){
+    console.log('uploaded a blob or file!')
+})
+
+console.log(storageRef)
